9e461ba1fbdde47234931cec2fc3b08fdb151735
Michael Deering <mdeering@mdeering.com>
2009-09-15 00:00:51 -0600

    Have the first round of configuration and documentation at a releasable point.

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+ActiveSupport::Dependencies.load_once_paths.delete File.dirname(__FILE__) + '/lib/gravitar_image_tag'
 require File.dirname(__FILE__) + '/lib/gravitar_image_tag'
 
 ActionView::Base.send(:include, GravitarImageTag)
