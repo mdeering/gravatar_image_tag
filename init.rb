f310533a710c8b3bbcb0539fb22c4852a0836838
Michael Deering <mdeering@mdeering.com>
2009-09-15 00:29:27 -0600

    removed the line that was causing the plugin to reload all of the time in dev mode.

--- init.rb
+++ init.rb
@@ -1,4 +1,3 @@
-ActiveSupport::Dependencies.load_once_paths.delete File.dirname(__FILE__) + '/lib/gravitar_image_tag'
 require File.dirname(__FILE__) + '/lib/gravitar_image_tag'
 
 ActionView::Base.send(:include, GravitarImageTag)
