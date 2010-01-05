a353d5f228a5bea4fae68177e02ba57f89cc07ee
Michael Deering <mdeering@mdeering.com>
2010-01-04 22:08:25 -0700

    File type support and doc changes.

--- lib/gravatar_image_tag.rb
+++ lib/gravatar_image_tag.rb
@@ -1,8 +1,8 @@
 module GravatarImageTag
 
   def self.included(base)
-    base.cattr_accessor :default_gravatar_image, :default_gravatar_rating,
-                        :default_gravatar_size,  :secure_gravatar
+    base.cattr_accessor :default_gravatar_filetype, :default_gravatar_image,
+      :default_gravatar_rating, :default_gravatar_size,  :secure_gravatar
     base.send :include, InstanceMethods
   end
 
@@ -10,26 +10,35 @@ module GravatarImageTag
 
     def gravatar_image_tag(email, options = {})
       options[:src] = gravatar_url( email, options.delete( :gravatar ) )
-      options[:alt] ||= File.basename(options[:src], '.*').split('.').first.to_s.capitalize
+      options[:alt] ||= 'Gravatar'
       tag 'img', options, false, false # Patch submitted to rails to allow image_tag here https://rails.lighthouseapp.com/projects/8994/tickets/2878-image_tag-doesnt-allow-escape-false-option-anymore
     end
 
     def gravatar_url(email, overrides)
       overrides ||= {}
-      url_params = {
+      gravatar_params = {
         :default     => ActionView::Base.default_gravatar_image,
-        :gravatar_id => Digest::MD5.hexdigest(email),
+        :filetype    => ActionView::Base.default_gravatar_filetype,
         :rating      => ActionView::Base.default_gravatar_rating,
         :secure      => ActionView::Base.secure_gravatar,
         :size        => ActionView::Base.default_gravatar_size
       }.merge(overrides).delete_if { |key, value| value.nil? }
-      "#{gravitar_url_base(url_params.delete(:secure))}?#{url_params.map { |key, value| "#{key}=#{URI.escape(value.is_a?(String) ? value : value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"}.join('&')}"
+      "#{gravatar_url_base(gravatar_params.delete(:secure))}/#{gravitar_id(email, gravatar_params.delete(:filetype))}#{url_params(gravatar_params)}"
     end
 
     private
 
-      def gravitar_url_base(secure = false)
-        'http' + (!!secure ? 's://secure.' : '://') + 'gravatar.com/avatar.php'
+      def gravatar_url_base(secure = false)
+        'http' + (!!secure ? 's://secure.' : '://') + 'gravatar.com/avatar'
+      end
+
+      def gravitar_id(email, filetype = nil)
+        "#{ Digest::MD5.hexdigest(email) }#{ ".#{filetype}" unless filetype.nil? }"
+      end
+
+      def url_params(gravatar_params)
+        return nil if gravatar_params.keys.count == 0
+        "?#{gravatar_params.map { |key, value| "#{key}=#{URI.escape(value.is_a?(String) ? value : value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"}.join('&')}"
       end
 
   end
--- spec/gravatar_image_tag_spec.rb
+++ spec/gravatar_image_tag_spec.rb
@@ -8,6 +8,7 @@ describe GravatarImageTag do
 
   email                 = 'mdeering@mdeering.com'
   md5                   = '4da9ad2bd4a2d1ce3c428e32c423588a'
+  default_filetype      = :gif
   default_image         = 'http://mdeering.com/images/default_gravatar.png'
   default_image_escaped = 'http%3A%2F%2Fmdeering.com%2Fimages%2Fdefault_gravatar.png'
   default_rating        = 'x'
@@ -27,14 +28,16 @@ describe GravatarImageTag do
     it "#gravatar_image_tag should create the provided url with the provided options #{options}"  do
       view = ActionView::Base.new
       image_tag = view.gravatar_image_tag(email, options)
+      image_tag.include?("#{params.delete(:gravatar_id)}").should be_true
       params.all? {|key, value| image_tag.include?("#{key}=#{value}")}.should be_true
     end
   end
 
   {
-    :default_gravatar_image  => default_image,
-    :default_gravatar_rating => default_rating,
-    :default_gravatar_size   => default_size
+    :default_gravatar_image    => default_image,
+    :default_gravatar_filetype => default_filetype,
+    :default_gravatar_rating   => default_rating,
+    :default_gravatar_size     => default_size
   }.each do |singleton_variable, value|
     it "should create gravatar class (singleton) variable #{singleton_variable} on its included class" do
       ActionView::Base.send(singleton_variable).should == nil
@@ -53,6 +56,7 @@ describe GravatarImageTag do
     it "#gravatar_image_tag should create the provided url when defaults have been set with the provided options #{options}"  do
       view = ActionView::Base.new
       image_tag = view.gravatar_image_tag(email, options)
+      image_tag.include?("#{params.delete(:gravatar_id)}.#{default_filetype}").should be_true
       params.all? {|key, value| image_tag.include?("#{key}=#{value}")}.should be_true
     end
   end
@@ -62,7 +66,7 @@ describe GravatarImageTag do
   end
 
   it 'should request the gravitar image from the secure server if the https => true option is given' do
-    (!!view.gravatar_image_tag(email, { :gravatar => { :secure => true } }).match(/src="https:\/\/secure.gravatar.com\/avatar.php/)).should be_true
+    (!!view.gravatar_image_tag(email, { :gravatar => { :secure => true } }).match(/src="https:\/\/secure.gravatar.com\/avatar\//)).should be_true
   end
 
 end
