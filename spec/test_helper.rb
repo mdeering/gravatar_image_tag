62c36ad0d2de5fba58888ccae1a9d0aefbe72801
Chris Your <chris@ignitionindustries.com>
2013-03-02 13:58:59 -0700

    Added Rspec tests for #gravatar_image_url.

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -6,6 +6,8 @@ require 'digest/md5'
 require 'uri'
 
 RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
